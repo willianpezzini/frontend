18ccfa91b9a5c6b41127ca50dd58e16ed92fb8dc
Willian Hercilio Pezzini <williantijucas@gmail.com>
2025-07-02 07:03:34 -0300

    Desenvolvendo o Fronte para a rota getAll

--- lib/api.ts
+++ lib/api.ts
@@ -1,11 +1,11 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
 export async function getUsers() {
-    const response = await fetch(`${API_URL}/users`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`);
 
     if (!response.ok) {
         throw new Error ('Erro ao buscar Usuários');
     }
 
-    return response.json();
+    const data = await response.json();
+    console.log("Dados Recebidos:", data);
+    return data;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,45 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { getUsers } from '../../lib/api';
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  phone?: string | null;
+}
+
 
 export default function Home() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getUsers().then(setUsers).catch(console.error);
+    async function fetchUsers() {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`);
+        const data = await response.json();
+
+        console.log("Usuarios recebidos", data);
+
+        if(Array.isArray(data)) {
+          setUsers(data);
+        }else {
+          console.log("A resposta não é um Array:", data)
+        }
+      } catch (error) {
+        console.error("A resposta não é um Array:", error)
+      } finally {
+        setLoading(false);
+      }
+
+    }
   }, []);
 
+
+  if (loading) return <h3>Carregando...</h3>;
+  
   return (
     <div>
       <h1>Lista de Usuários consumidos da api criada</h1>
